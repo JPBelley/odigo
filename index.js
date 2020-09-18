7cc23f83949d3602b6c0a313b802dfc61f6152e9
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2020-09-17 23:43:33 -0400

    Mobile done

--- index.js
+++ index.js
@@ -3,40 +3,34 @@ import './sass/style.scss';
 
 const init = () => {
 
-	
+	const isLegit = (email) => {
+		const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
+
 	const validateEmail = (form) => {
-
-		const isLegit = (email) => {
-			const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-			return re.test(String(email).toLowerCase());
-		}
-
+		
 		const emailSubmitButton = form.querySelector('button');
 	
 		if (emailSubmitButton) {
 			emailSubmitButton.addEventListener('click', (e) => {
 				e.preventDefault(e);
 				const emailInput = form.querySelector('input');
-				const formError = form.querySelector('.form-error');
-				if (formError) formError.remove();
 				const email = emailInput.value;
+				const formError = form.querySelector('.form-error');
+				formError.classList.remove('show'); 
+
 				const isEmailLegit = isLegit(email);
-				const error = document.createElement("div");
-				const text = document.createTextNode('Entrer un courriel valide');
-				error.appendChild(text);
-				error.classList.add('form-error');
 
 				isEmailLegit ? 
-					form.submit() : // Surement un query ajax
-					form.appendChild(error); 
-
-				// isLegit ? 
+					form.submit() : // Surement une query ajax
+					formError.classList.add('show'); 
 			});
 		}
 	}
 
 	const formEmail = document.getElementById('formEmail');
-	validateEmail(formEmail);
+	if (formEmail) validateEmail(formEmail);
 }
 
 init();
